93583e1a0dbbe572f649bd1ab1338c57f776c707
Alex Gusev <alex@flancer64.com>
2018-06-15 11:37:37 +0300

    mage2_app_probion-52 Currency conversion broken
    Related to: flancer32/mage2_app_probion#52

---
+++ Helper/Config.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Authors: Alex Gusev <alex@flancer64.com>
+ * Since: 2018
+ */
+
+namespace Flancer32\FreeRates\Helper;
+
+
+class Config
+{
+
+    /** @var \Magento\Framework\App\Config\ScopeConfigInterface */
+    private $scopeConfig;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDelay()
+    {
+        $result = (int)$this->scopeConfig->getValue('currency/fcc/delay');
+        return $result;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimeout()
+    {
+        $result = (int)$this->scopeConfig->getValue('currency/fcc/timeout');
+        return $result;
+    }
+}
--- Model/Currency/Import/Fcc.php
+++ Model/Currency/Import/Fcc.php
@@ -10,6 +10,67 @@ namespace Flancer32\FreeRates\Model\Currency\Import;
 
 
 class Fcc
+    extends \Magento\Directory\Model\Currency\Import\AbstractImport
 {
+    /**
+     * @var string
+     */
+    const CURRENCY_CONVERTER_URL = 'https://free.currencyconverterapi.com/api/v3/convert?q={{CURRENCY_FROM}}_{{CURRENCY_TO}}';
 
+    /** @var \Flancer32\FreeRates\Helper\Config */
+    private $hlpCfg;
+    /** @var \Magento\Framework\HTTP\ZendClient */
+    private $httpClient;
+    /** @var \Magento\Framework\Json\Helper\Data */
+    private $jsonHelper;
+
+    public function __construct(
+        \Magento\Framework\HTTP\ZendClient $httpClient,
+        \Magento\Directory\Model\CurrencyFactory $currencyFactory,
+        \Magento\Framework\Json\Helper\Data $jsonHelper,
+        \Flancer32\FreeRates\Helper\Config $hlpCfg
+    ) {
+        parent::__construct($currencyFactory);
+        $this->httpClient = $httpClient;
+        $this->jsonHelper = $jsonHelper;
+        $this->hlpCfg = $hlpCfg;
+    }
+
+    /**
+     * @param string $currencyFrom
+     * @param string $currencyTo
+     * @param int $retry
+     * @return float|null
+     */
+    protected function _convert($currencyFrom, $currencyTo, $retry = 0)
+    {
+        $result = null;
+        $url = str_replace('{{CURRENCY_FROM}}', $currencyFrom, self::CURRENCY_CONVERTER_URL);
+        $url = str_replace('{{CURRENCY_TO}}', $currencyTo, $url);
+        $delay = $this->hlpCfg->getDelay();
+        $timeout = $this->hlpCfg->getTimeout();
+        try {
+            sleep($delay);
+            $this->httpClient->setUri($url);
+            $this->httpClient->setConfig(['timeout' => $timeout,]);
+            $response = $this->httpClient->request('GET');
+            $body = $response->getBody();
+            $resultKey = $currencyFrom . '_' . $currencyTo;
+            $data = $this->jsonHelper->jsonDecode($body);
+            $results = $data['results'][$resultKey];
+            $queryCount = $data['query']['count'];
+            if (!$queryCount && !isset($results)) {
+                $this->_messages[] = __('We can\'t retrieve a rate from %1.', $url);
+            } else {
+                $result = (float)$results['val'];
+            }
+        } catch (\Exception $e) {
+            if ($retry == 0) {
+                $this->_convert($currencyFrom, $currencyTo, 1);
+            } else {
+                $this->_messages[] = __('We can\'t retrieve a rate from %1.', $url);
+            }
+        }
+        return $result;
+    }
 }
